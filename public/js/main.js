d35f103ef02d78534a0e0e6313c6c92e62fb843d
selestinohajaniaina <selestinoolivier@gmail.com>
2023-01-22 20:59:05 +0300

    new feature

--- public/js/main.js
+++ public/js/main.js
@@ -4,20 +4,14 @@ let url = "http://api.weatherstack.com/current",
   access_key = "7c29b6a021546add5ea47e74f9025871",
   units = "m",
   ville_default = "Mahajanga";
-const select = document.querySelector("#select");
-const btn = document.querySelector("#btn");
-btn.addEventListener("click", () => {
-  alert(1);
-});
 
 request(
   `${url}?access_key=${access_key}&query=${ville_default}&units=${units}`,
   (err, res, body) => {
-    data = JSON.parse(body);
-    const { request, location, current } = data;
-    console.log(location + "\n" + current);
-    // console.log(
-    //   `ici c'est ${location.name} a ${location.localtime} pour temperature de ${current.temperature} degre`
-    // );
+    let data = JSON.parse(body);
+    let { request, location, current } = data;
+    console.log(
+      `Name: ${location.name} \n country:${location.country} \n time: ${location.localtime} \n degree: ${current.temperature} degre`
+    );
   }
 );
